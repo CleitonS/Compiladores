66e12613c500a2209603a2e8f0f52d6df89dcac7
CleitonS <cleiton1302@gmail.com>
2018-07-16 15:16:46 -0300

    Update

--- Etapa6/asmData.s
+++
@@ -1,9 +0,0 @@
-.data
-a:	.long	10
-b:	.long	10
-c:	.long	100
-um:	.long	1
-a1:	.long	10
-a2:	.long	10
-_VarTemp8:	.long	1
-_VarTemp9:	.long	2
--- Etapa6/asmRData.s
+++
@@ -1,2 +0,0 @@
-.def	__main;	.scl	2;	.type	32;	.endef
-.section .rdata,"dr"
